d353609bf58b91d400a3a98afd23bb4788a66efe
BlancoFS <60780075+BlancoFS@users.noreply.github.com>
2022-11-07 09:55:40 +0100

    Create Winter22_122X_nAODv10.py

---
+++ NanoGardener/python/framework/samples/Winter22_122X_nAODv10.py
@@ -0,0 +1,27 @@
+'''                                                                                                                                                                                                                                           
+Samples = {                                                                                                                                                                                                                                   
+              'GluGluHToWWTo2L2Nu_M125' : {                                                                                                                                                                                                   
+                   'dasInst' : 'prod/global'  ,                                                                                                                                                                                               
+                   'nanoAOD' : '/GluGluHToWWTo2L2Nu_M125_13TeV_powheg_pythia8/RunIISummer16NanoAOD-PUMoriond17_05Feb2018_94X_mcRun2_asymptotic_v2-v1/NANOAODSIM' ,                                                                            
+                                          } ,                                                                                                                                                                                                 
+                                                                                                                                                                                                                                              
+              'GluGluHToWWTo2L2Nu_M125_Crab' : {                                                                                                                                                                                              
+                   'dasInst' : 'prod/global'  ,                                                                                                                                                                                               
+                   'nanoAOD' : '/GluGluHToWWTo2L2Nu_M125_13TeV_powheg_pythia8/RunIISummer16NanoAOD-PUMoriond17_05Feb2018_94X_mcRun2_asymptotic_v2-v1/NANOAODSIM' ,                                                                            
+                                          } ,                                                                                                                                                                                                 
+                                                                                                                                                                                                                                              
+               'GluGluHToWWTo2L2Nu_M125_Private' : {                                                                                                                                                                                          
+                    'dasInst' : 'prod/phys03'  ,                                                                                                                                                                                              
+                    'nanoAOD' : '/GluGluHToWWTo2L2Nu_M125_13TeV_powheg_pythia8/ddicroce-GluGluHToWWTo2L2Nu_M125_13TeV_powheg_pythia8_nAOD_private-60fc7d9153a3c626dca414ab6cce3b8f/USER',                                                     
+                                           } ,                                                                                                                                                                                                
+                                                                                                                                                                                                                                              
+ }                                                                                                                                                                                                                                            
+'''
+
+Samples = {}
+
+Samples['WW_TuneCP5_13p6TeV-pythia8'] = {'nanoAOD': '/WW_TuneCP5_13p6TeV-pythia8/Run3Winter22NanoAOD-122X_mcRun3_2021_realistic_v9-v1/NANOAODSIM'}
+Samples['TTTo2L2Nu'] = {'nanoAOD': '/TTTo2L2Nu_CP5_13p6TeV_powheg-pythia8/Run3Winter22NanoAOD-122X_mcRun3_2021_realistic_v9-v1/NANOAODSIM'}
+Samples['DYJetsToLL_M-10to50'] = {'nanoAOD': '/DYJetsToLL_M-10to50_TuneCP5_13p6TeV-madgraphMLM-pythia8/Run3Winter22NanoAOD-122X_mcRun3_2021_realistic_v9-v1/NANOAODSIM'}
+Samples['WZ'] = {'nanoAOD': '/WZ_TuneCP5_13p6TeV-pythia8/Run3Winter22NanoAOD-122X_mcRun3_2021_realistic_v9-v1/NANOAODSIM'}
+Samples['ZZ'] = {'nanoAOD': '/ZZ_TuneCP5_13p6TeV-pythia8/Run3Winter22NanoAOD-122X_mcRun3_2021_realistic_v9-v1/NANOAODSIM'}
